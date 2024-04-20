c4a107587f4cf6efcf9904a7a1bb892cb0aad237
Vitaly <82165471+VKrivkov@users.noreply.github.com>
2024-04-20 17:31:54 +0200

    webp mainpage

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,27 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import './MainPage.css'; // Ensure your CSS is correctly linked
-import image1 from '../../assets/ShowImage1.png';
-import image2 from '../../assets/ShowImage2.png';
-import image3 from '../../assets/ShowImage3.png';
-import image4 from '../../assets/ShowImage4.png';
-import image5 from '../../assets/ShowImage5.png';
-import image6 from '../../assets/ShowImage6.png';
-import image7 from '../../assets/ShowImage7.png';
-import image8 from '../../assets/ShowImage8.png';
-import image9 from '../../assets/ShowImage9.png';
-import image10 from '../../assets/ShowImage10.png';
-import image11 from '../../assets/ShowImage11.png';
-import image12 from '../../assets/ShowImage12.png';
-
-
-
-
-
+import image1 from '../../assets/ShowImage1.webp';
+import image2 from '../../assets/ShowImage2.webp';
+import image3 from '../../assets/ShowImage3.webp';
+import image4 from '../../assets/ShowImage4.webp';
+import image5 from '../../assets/ShowImage5.webp';
+import image6 from '../../assets/ShowImage6.webp';
+import image7 from '../../assets/ShowImage7.webp';
+import image8 from '../../assets/ShowImage8.webp';
+import image9 from '../../assets/ShowImage9.webp';
+import image10 from '../../assets/ShowImage10.webp';
+import image11 from '../../assets/ShowImage11.webp';
+import image12 from '../../assets/ShowImage12.webp';
 
 const MainPage = () => {
   // Initial images array
   const images = [image1, image9, image5, image4, image3, image6, image7, image8, image10, image2, image11, image12];
-
 
   // State to track which images have been revealed
   const [revealed, setRevealed] = useState(Array(images.length).fill(false));
@@ -29,7 +23,7 @@ const MainPage = () => {
 
   // Function to randomly assign a grid size to each image
   const getSize = (index) => {
-    if (index<8)
+    if (index < 8)
       return gridSizes[index % 4];
     else if (index === 9)
       return 'size-3';
@@ -55,8 +49,6 @@ const MainPage = () => {
     };
 
     revealImages();
-
-
   }, [images.length]);
 
   return (
@@ -68,10 +60,10 @@ const MainPage = () => {
           <h3>IDEAS</h3>
         </div>
         <div className='main-page-fun'>
-          <p>-Our 3d printing studio is here to print any of your ideas.</p> 
+          <p>-Our 3d printing studio is here to print any of your ideas.</p>
           <div className='two-things'>
-            <p> -Any ideas?  </p>  
-            <strong> -All of them.</strong>
+            <p>-Any ideas?</p>
+            <strong>-All of them.</strong>
           </div>
         </div>
       </header>
@@ -79,7 +71,7 @@ const MainPage = () => {
         {images.map((image, index) => (
           <img
             key={index}
-            className={`bento-image (${(index)}) ${getSize(index)} ${revealed[index] ? 'reveal' : ''}`}
+            className={`bento-image (${index}) ${getSize(index)} ${revealed[index] ? 'reveal' : ''}`}
             src={image}
             loading='lazy'
             alt={`3D printing image ${index + 1}`}
@@ -87,10 +79,10 @@ const MainPage = () => {
         ))}
       </div>
       <div className='imgBox-medium'>
-      {images.slice(0, 6).map((image, index) => (
+        {images.slice(0, 6).map((image, index) => (
           <img
             key={index}
-            className={`bento-image (${(index)}) ${getSize(index)} ${revealed[index] ? 'reveal' : ''}`}
+            className={`bento-image (${index}) ${getSize(index)} ${revealed[index] ? 'reveal' : ''}`}
             src={image}
             loading='lazy'
             alt={`3D printing image ${index + 1}`}
@@ -98,10 +90,10 @@ const MainPage = () => {
         ))}
       </div>
       <div className='imgBox-small'>
-      {images.slice(0, 4).map((image, index) => (
+        {images.slice(0, 4).map((image, index) => (
           <img
             key={index}
-            className={`bento-image (${(index)}) ${getSize(index)} ${revealed[index] ? 'reveal' : ''}`}
+            className={`bento-image (${index}) ${getSize(index)} ${revealed[index] ? 'reveal' : ''}`}
             src={image}
             loading='lazy'
             alt={`3D printing image ${index + 1}`}
