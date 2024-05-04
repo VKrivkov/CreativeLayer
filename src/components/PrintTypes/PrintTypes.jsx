f36d5dc58eca26115e472d5a6fb19e5c862d152f
Vitaly <82165471+VKrivkov@users.noreply.github.com>
2024-05-04 18:23:11 +0200

    Update PrintTypes.jsx

--- src/components/PrintTypes/PrintTypes.jsx
+++ src/components/PrintTypes/PrintTypes.jsx
@@ -8,7 +8,7 @@ const PrintTypes = () => {
     <div className='print-types' id='types'>
         <div className='sla-container'>
             <div className='sla-text-container'> 
-              <h2>What is SLA 3D Printing</h2>
+              <h2>What is SLA 3D Printing?</h2>
               <p>SLA (Stereolithography) 3D Printing uses ultraviolet light to harden precisely liquid resin into high-quality solid objects. SLA produces detailed models with smooth finishes.</p>
               <p><strong>Examples of implementation:</strong> miniatures and figurines, highly detailed models an mechanisms, jewelry</p>
               <ul>
@@ -29,7 +29,7 @@ const PrintTypes = () => {
             <img src={FDM} alt='FDM printer' loading='lazy'/>
           </div>
           <div className='fdm-text-container'> 
-              <h2>What is FDM 3D Printing</h2>
+              <h2>What is FDM 3D Printing?</h2>
               <p>FDM (Fused Deposition Modeling) 3D Printing builds objects by heating and extruding plastic through a small nozzle, layer by layer. FDM produces durable objects using a variety of materials.</p>
               <p><strong>Examples of application:</strong> prototypes, casings and shells, cosplay costume elements</p>
               <ul>
